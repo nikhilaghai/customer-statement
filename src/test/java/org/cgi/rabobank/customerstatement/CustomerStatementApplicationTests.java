d64e246991fee263b51dfce43ab576420e4c57d4
nikhila.ghai <nikhila.ghai@cgi.com>
2019-05-21 09:33:02 +0200

    Initial commit

---
+++ src/test/java/org/cgi/rabobank/customerstatement/CustomerStatementApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cgi.rabobank.customerstatement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerStatementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
